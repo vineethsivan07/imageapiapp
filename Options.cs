bf89321aba16164d6d5c8c907662fdee93a1bd35
vineethsivan07 <96585509+vineethsivan07@users.noreply.github.com>
2021-12-24 20:02:58 +0530

    commit

---
+++ Options.cs
@@ -0,0 +1,11 @@
+namespace Api
+{
+    public class Options
+    {
+        public string StorageConnectionString { get; set; }
+
+        public string FullImageContainerName { get; set; }
+
+        public string ThumbnailImageContainerName { get; set; }
+    }
+}
